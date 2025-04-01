971da3e786df96b6c73ffce6c5f9eb3c0537bd9d
RuxandraUrs <ruxandraurs@gmail.com>
2025-04-01 14:39:34 +0300

    added empty proj

---
+++ src/app/components/vinyl-list/vinyl-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vinyl-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './vinyl-list.component.html',
+  styleUrl: './vinyl-list.component.scss'
+})
+export class VinylListComponent {
+
+}
