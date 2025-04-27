0e89e4398a60ef5320a002f9bb9af3acddcdd113
GabyTheNephew <gabriel.slav@student.unitbv.ro>
2025-04-27 20:51:06 +0300

    The whole project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,22 @@ import { VinylListComponent } from "./components/vinyl-list/vinyl-list.component
 import { WelcomeComponent } from "./components/welcome/welcome.component";
 import { CdListComponent } from "./components/cd-list/cd-list.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { VinylComponent } from "./components/vinyl/vinyl.component";
+import { HomePageComponent } from "./components/home-page/home-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FeaturesComponent, HeaderComponent, VinylListComponent, WelcomeComponent, CdListComponent, FooterComponent],
+  imports: [FeaturesComponent, HeaderComponent, VinylListComponent, WelcomeComponent, CdListComponent, FooterComponent, VinylComponent, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'music-shop';
+
+  albumTitle: string = '';
+
+  onSelected(albumTitle: string) {
+    this.albumTitle = albumTitle;
+  }
 }
